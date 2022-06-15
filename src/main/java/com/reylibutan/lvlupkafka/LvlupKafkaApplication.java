13c59dba0273a5c051912ab3a865363a4c9f0cb6
Rey Libutan <reylibutan@gmail.com>
2022-06-15 11:20:40 +0800

    Initial commit

---
+++ src/main/java/com/reylibutan/lvlupkafka/LvlupKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.reylibutan.lvlupkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvlupKafkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LvlupKafkaApplication.class, args);
+  }
+}
---
+++ src/test/java/com/reylibutan/lvlupkafka/LvlupKafkaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.reylibutan.lvlupkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvlupKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
